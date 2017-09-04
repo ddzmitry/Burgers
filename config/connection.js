e018f62cf7aac09687406f97038c26db64c460e0
Dzmitry A Dubarau <ddzmitry@yahoo.com>
2017-09-04 10:26:31 -0400

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -10,7 +10,7 @@ if (process.env.JAWSDB_URL) {
         port: 3306,
         host: "localhost",
         user: "root",
-        password: "Kainer2305!",
+        password: "root",
         database: "burgers_db"
     });
 
